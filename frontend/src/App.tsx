aeecd03438f645f76c9550ea167a59a79a058705
Restu Dwi Cahyo <restu@resitdc.id>
2026-04-20 21:27:49 +0700

    Update frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,54 @@
-import { useState, useEffect } from "react";
-import { EventsOn } from "../wailsjs/runtime/runtime";
-import "./App.css";
+import { useState, useEffect, useRef } from 'react';
+import './App.css';
 
-const App = () => {
-	const [barcode, setBarcode] = useState<string>("Belum ada data");
-	const [scanCount, setScanCount] = useState<number>(0);
+function App() {
+    const [barcode, setBarcode] = useState<string>("Belum ada data");
+    
+    const buffer = useRef<string>(""); 
+    const lastKeyTime = useRef<number>(Date.now());
 
-	useEffect(() => {
-		EventsOn("on-barcode-scanned", (data: string) => {
-			setBarcode(data);
-			setScanCount((prev) => prev + 1);
-		});
-	}, []);
+    useEffect(() => {
+        const handleKeyDown = (e: KeyboardEvent) => {
+            const currentTime = Date.now();
+            const timeDiff = currentTime - lastKeyTime.current; 
+            
+            if (timeDiff > 50) {
+                buffer.current = "";
+            }
 
-	return (
-		<div id="App">
-			<h1>Guest Barcode Scanner</h1>
-			<div className="status-box">
-				<h2>Data Masuk:</h2>
-				<h1 style={{ color: "#00ff00", fontSize: "40px" }}>{barcode}</h1>
-				<p>Total Barcode Discan: {scanCount}</p>
-			</div>
-		</div>
-	)
+            if (e.key.length === 1) {
+                buffer.current += e.key;
+            }
+
+            if (e.key === "Enter" && buffer.current.length > 3) {
+                const finalBarcode = buffer.current;
+                console.log("Scanner berhasil membaca:", finalBarcode);
+                
+                setBarcode(finalBarcode);
+                
+                
+                buffer.current = "";
+            }
+
+            lastKeyTime.current = currentTime;
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+
+    return (
+        <div id="App">
+            <h1>Sistem Operator Scanner (Mode Keyboard)</h1>
+            <div className="status-box">
+                <h2>Data Masuk:</h2>
+                <h1 style={{ color: '#00ff00', fontSize: '40px' }}>{barcode}</h1>
+            </div>
+        </div>
+    )
 }
 
 export default App;
